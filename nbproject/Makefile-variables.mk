00af3551c6d318c52539e2bd2abed01a68db0bc3
eduardoYan <eduardoyan@unifei.edu.br>
2021-11-29 14:11:57 -0300

    Exercicio3Micro

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Questao3.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Questao3.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=questao3.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/questao3.x.tar
